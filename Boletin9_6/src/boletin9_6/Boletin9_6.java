e01db92d255038ff3289b03480be7d2f11afa3a4
clamascabaleiro <clamascabaleiro@nautilus15.danielcastelao.org>
2018-11-19 10:55:14 +0100

    Ejercicio Terminado

---
+++ Boletin9_6/src/boletin9_6/Boletin9_6.java
@@ -0,0 +1,17 @@
+
+
+package boletin9_6;
+
+/**
+ *
+ * @author clamascabaleiro
+ */
+public class Boletin9_6 {
+
+ 
+    public static void main(String[] args) {
+       Sueldos numero = new Sueldos();
+       numero.lecturaSueldos();
+    }
+
+}
---
+++ Boletin9_6/src/boletin9_6/Sueldos.java
@@ -0,0 +1,31 @@
+package boletin9_6;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author clamascabaleiro
+ */
+public class Sueldos {
+    public void lecturaSueldos(){
+        double soldo; 
+        int contadorTrabajadores = 0;
+        int contadorSueldos_1 = 0;
+        int contadorSueldos_2 = 0;
+        do{
+            soldo = Double.parseDouble(JOptionPane.showInputDialog("Introduzca el sueldo de un empleado:"));
+            if(soldo > 0){
+                contadorTrabajadores++;
+            }
+            if(soldo >= 1000 && soldo <= 1750){
+                contadorSueldos_1++;
+            }
+            else if(soldo < 1000 && soldo > 0){
+                contadorSueldos_2++;
+            }
+        }while(soldo != 0);
+        JOptionPane.showMessageDialog(null, "Número de trabajadores que ganan entre 1000 y 1750€: " 
+                + contadorSueldos_1 + "\nPorcentaje de trabajadores que ganan menos de 1000€: " 
+                + (contadorSueldos_2*100/contadorTrabajadores));
+    }
+}
