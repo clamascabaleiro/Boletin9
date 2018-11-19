da75f95598e9b96bfce57b02c2c29515af6940de
clamascabaleiro <clamascabaleiro@nautilus15.danielcastelao.org>
2018-11-19 10:37:48 +0100

    Ejercicio Terminado

---
+++ Boletin9_5/src/boletin9_5/Boletin9_5.java
@@ -0,0 +1,19 @@
+
+
+package boletin9_5;
+
+/**
+ *
+ * @author clamascabaleiro
+ */
+public class Boletin9_5 {
+
+ 
+    public static void main(String[] args) {
+       Serie numero = new Serie();
+//       numero.serieSuma2();  //Apartado a)
+//       numero.serieSumaResta(); //Apartado b)
+       numero.serieSuma(); //Apartado c)
+    }
+
+}
